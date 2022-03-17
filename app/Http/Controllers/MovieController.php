314b2352f286299ca457efc4264cecbbf3f3acdb
SangesAntonio <sanges65@gmail.com>
2022-03-17 15:08:04 +0100

    created project

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+
+class MovieController extends Controller
+{
+    //movies
+    public function index()
+    {
+       $movies =
+    }
+}
